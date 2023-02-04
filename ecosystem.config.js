115b3e91704e4674b1c94c8d89a99aac9a3a17a4
morteza-jamali <mortezajamali4241@gmail.com>
2023-02-04 20:32:41 +0330

    chore(base): improve `ecosystem.config.js` config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,30 @@
+const shared_opts = {
+  autorestart: false,
+  time: true,
+};
+
+const apps = [
+  {
+    name: 'picsum:serve',
+    script: './scripts/picsum/picsum.js',
+  },
+  {
+    name: 'frontend:build:development',
+    script: './node_modules/nx/bin/nx.js',
+    args: 'run frontend-frontend:build:development --watch',
+  },
+  {
+    name: 'plugins:storybook',
+    script: './node_modules/nx/bin/nx.js',
+    args: 'run plugins:storybook',
+  },
+  {
+    name: 'backend:serve',
+    script: './node_modules/nx/bin/nx.js',
+    args: 'serve backend',
+  },
+];
+
 module.exports = {
-  apps: [
-    {
-      name: 'picsum:serve',
-      script: './scripts/picsum/picsum.js',
-      autorestart: false,
-    },
-    {
-      name: 'frontend:build:development',
-      script: './node_modules/nx/bin/nx.js',
-      args: 'run frontend-frontend:build:development --watch',
-      autorestart: false,
-    },
-    {
-      name: 'backend:serve',
-      script: './node_modules/nx/bin/nx.js',
-      args: 'serve backend',
-      autorestart: false,
-    },
-  ],
+  apps: apps.map((app) => ({ ...shared_opts, ...app })),
 };
