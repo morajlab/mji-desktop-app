b0f776d0f34bbb58cd495b0a310f06ad6f87c27e
morteza-jamali <mortezajamali4241@gmail.com>
2023-02-08 04:28:14 +0330

    chore(packages): create `convertOptions()` function in `@master/frontend:bundle` executor

--- packages/frontend/tools/tools/src/executors/bundle/executor.ts
+++ packages/frontend/tools/tools/src/executors/bundle/executor.ts
@@ -3,11 +3,27 @@ import { execSync } from 'child_process';
 
 import type { BundleExecutorSchema } from './schema';
 
+const convertOptions = (options: BundleExecutorSchema): string =>
+  Object.entries(options)
+    .map(([key, value]) => {
+      switch (typeof value) {
+        case 'boolean':
+        case 'number':
+          return [key, String(value)];
+        case 'object':
+          return [key, JSON.stringify(value)];
+        default:
+          return [key, value];
+      }
+    })
+    .map(([key, value]) => `--${key}='${value}'`)
+    .join(' ');
+
 export default async function bundleExecutor(options: BundleExecutorSchema) {
   try {
-    console.log(options);
-
-    const result = execSync(`go run ${join(__dirname, 'executor.go')}`);
+    const result = execSync(
+      `go run ${join(__dirname, 'executor.go')} ${convertOptions(options)}`
+    );
     console.log(String(result));
 
     return { success: true };
